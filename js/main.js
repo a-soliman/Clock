ce01be29a8c40f9054aee37da446cfa83a211977
Ahmed Soliman <ahmedanwar@mail.com>
2016-07-24 21:40:19 -0700

    modified the simple program to output each part on its own div

--- js/main.js
+++ js/main.js
@@ -7,7 +7,13 @@ today.getSeconds()
 var clock = today.getHours() +' ' + today.getMinutes() + ' ' + today.getSeconds() 
 
 var el = document.getElementById('body')
+var hoursDiv = document.getElementById('hours')
+var minutesDiv = document.getElementById('minutes')
+var secondesDiv = document.getElementById('secondes')
+
 function domListenerEvent() {
-	el.textContent = clock
+	hoursDiv.textContent = today.getHours() + ' :'
+	minutesDiv.textContent = today.getMinutes() + ' :'
+	secondesDiv.textContent = today.getSeconds()
 }
 el.addEventListener('onload', domListenerEvent(), false)
