8bb3b77d083badb317365c79d3cfcf088860a4af
Ahmed Soliman <ahmedanwar@mail.com>
2016-07-24 21:57:25 -0700

    modified the progrem to work with 'setTimeout'

--- js/main.js
+++ js/main.js
@@ -1,19 +1,27 @@
 
 //storing the vars
+
+var el = document.getElementById('body')
+
+function time() {
 var today = new Date()
 today.getHours()
 today.getMinutes()
 today.getSeconds()
 var clock = today.getHours() +' ' + today.getMinutes() + ' ' + today.getSeconds() 
 
-var el = document.getElementById('body')
 var hoursDiv = document.getElementById('hours')
 var minutesDiv = document.getElementById('minutes')
 var secondesDiv = document.getElementById('secondes')
 
-function domListenerEvent() {
+
 	hoursDiv.textContent = today.getHours() + ' :'
 	minutesDiv.textContent = today.getMinutes() + ' :'
 	secondesDiv.textContent = today.getSeconds()
+
+setTimeout(function() {
+	time() }, 1000);
 }
-el.addEventListener('onload', domListenerEvent(), false)
+
+ el.addEventListener('onload', time(), false)
+
